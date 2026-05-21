ca2e9bdd047566cf07b58543a134b607a68dc1d5
Treasure_Mei_Box <yadanartheint2412@gmail.com>
2026-05-21 14:55:59 +0800

    modified test case of Map 2 with A* search

--- tests/test_map2.py
+++ tests/test_map2.py
@@ -68,11 +68,11 @@ class TestMap2:
     def test_astar(self):
         """Test A* with various origin-destination pairs."""
         test_cases = [
-            {"origin": 1, "destinations": [11], "expected_path": [], "expected_cost": 0, "expected_nodes": 0},
-            {"origin": 3, "destinations": [11], "expected_path": [], "expected_cost": 0, "expected_nodes": 0},
-            {"origin": 6, "destinations": [9], "expected_path": [], "expected_cost": 0, "expected_nodes": 0},
-            {"origin": 5, "destinations": [11], "expected_path": [], "expected_cost": 0, "expected_nodes": 0},
-            {"origin": 2, "destinations": [9, 11], "expected_path": [], "expected_cost": 0, "expected_nodes": 0},
+            {"origin": 1, "destinations": [11], "expected_path": [1,3,5,7,9,11], "expected_cost": 28, "expected_nodes": 13},
+            {"origin": 3, "destinations": [11], "expected_path": [3,5,7,9,11], "expected_cost": 22, "expected_nodes": 8},
+            {"origin": 6, "destinations": [9], "expected_path": [6,7,9], "expected_cost": 14, "expected_nodes": 7},
+            {"origin": 5, "destinations": [11], "expected_path": [5,7,9,11], "expected_cost": 17, "expected_nodes": 7},
+            {"origin": 2, "destinations": [9, 11], "expected_path": [2,4,8,9], "expected_cost": 21, "expected_nodes": 10},
         ]
 
         astar = AStar(self.graph)
